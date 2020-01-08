fac4b528c870a84a3c4e5ea1633d5ce9b0d8915c
zhalice2011 <zhalice2012@gmail.com>
2020-01-08 20:47:57 +0800

    add jquery find methods

--- jquery.mini.js
+++ jquery.mini.js
@@ -8,10 +8,94 @@
         return new jQuery.fn.init(selector);
     };
 
+    var obj = {},
+      toStr = obj.toString,
+      hasOwnProp = obj.hasOwnProperty,
+      arr = [],
+      slice = arr.slice,
+      push = arr.push;
+
+    var isObject = function(obj) {
+        return obj == null ? obj.toString() : toStr.call(obj) === '[object Object]';
+    },
+    isArray = Array.isArray || function(obj) {
+        return toStr.call(obj) === '[object Array]';
+    },
+    isFunction = function(obj) {
+        return typeof obj === 'function';
+    },
+    isString = function(obj) {
+        return typeof obj === 'string';
+    },
+    isBoolean = function(obj) {
+        return typeof obj === 'boolean';
+    },
+    inArray = function(arr, item) {
+        return arr.indexOf(item) !== -1;
+    };
+
+    function pushStack(target) {
+        var len = target.length;
+  
+        if (typeof len !== 'number' || len < 0) {
+            //没有length属性，或者它小于0，直接返回target
+            return target;
+        }
+  
+        //保存除target外的所有参数并转化为数组形式
+        var sources = slice.call(arguments, 1);
+  
+        each(sources, function() {
+            //this值就是sources数组中的每一项
+            var len = this.length;
+  
+            //同样判断是否为数组或类数组
+            if (typeof len !== 'number' || len < 0) {
+                return;
+            }
+  
+            //之前保存的常用函数之一push，派上用场
+            //与apply结合，批量push进target中
+            push.apply(target, this);
+        });
+        return target;
+    }
+
+    function each(obj, callback) {
+        //如果既不是对象也不是数组，不迭代，直接返回
+        if (!isObject(obj) && !isArray(obj)) {
+            return obj;
+        }
+        var len = obj.length;
+        //要求obj.length类型为数字且不小于0
+        //符合该条件就当做数组来迭代
+        if (typeof len === 'number' && len >= 0) {
+            //如果有用户滥用length属性，导致错误是其活该
+            for (var i = 0; i < len; i += 1) {
+                //callback函数的this值为数组中的每一项
+                if (callback.call(obj[i], i, obj[i]) === false) {
+                    //如果callback函数的返回值等价于false
+                    //终止迭代
+                    break;
+                }
+            }
+        } else {
+            //程序运行到这一步，说明该迭代的Obj类型为object
+            for (var prop in obj) {
+                if (hasOwnProp.call(obj, prop)) {
+                    if (callback.call(obj[prop], prop, obj[prop]) === false) {
+                        break;
+                    }
+                }
+            }
+        }
+        //返回被迭代的数组或对象
+        return obj;
+    }
 
     jQuery.fn = jQuery.prototype = {
         init: function (selector) {
-            if (selector.nodeType) {
+            if (selector && selector.nodeType) {
                 this[0] = selector;
                 this.length = 1;
                 return this;
@@ -40,6 +124,19 @@
 
 
     jQuery.extend({
+        merge: function( first, second ) {
+            var len = +second.length,
+                j = 0,
+                i = first.length;
+    
+            for ( ; j < len; j++ ) {
+                first[ i++ ] = second[ j ];
+            }
+    
+            first.length = i;
+    
+            return first;
+        },
         trim: function (text) {
             return (text || "").replace(/^\s+|\s+$/g, "");
         },
@@ -55,6 +152,12 @@
 
 
     jQuery.fn.extend({
+
+        
+        //each在这里很简单
+        easyEach: function(callback) {
+            return each(this, callback);
+        },
         get: function (num) {
             return this[num];
         },
@@ -96,6 +199,7 @@
                 added = ele_class + blank + name;
                 ele.className = added;
             });
+            return this;
         },
         removeClass: function () { //删除(class)类
             var name = arguments[0];
@@ -159,8 +263,16 @@
                 for (var i = 0; i < ele.children.length; i++) {
                     var ss = ele.children[i];
                 }
-
             });
+        },
+        find: function(selector) {
+            var ret = $();
+            var nodes = [];
+            this.easyEach(function() {
+                var items = this.querySelectorAll(selector);
+                items.length && push.apply(nodes, items);
+            });
+            return pushStack(ret, nodes);
         },
 
     });
